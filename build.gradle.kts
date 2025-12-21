5cc41d6494bc4f7ecddaf72270a7aa11e2151a88
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-21 16:15:12 +0100

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    java
+    id("io.github.juuxel.loom-quiltflower") version "1.10.0"
+    id("maven-publish")
+}
+
+group = "net.lux"
+version = "0.1.0-alpha"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.fabricmc.net/")
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:1.20.1") 
+    
+    mappings(loom.officialMojangMappings()) 
+
+    implementation("org.spongepowered:mixin:0.8.5")
+}
+
+tasks.withType<JavaCompile> {
+    options.release.set(17)
+}

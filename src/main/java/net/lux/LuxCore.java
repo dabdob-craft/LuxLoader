5246eeed6e8de41d3a8bc5616669437d584de48b
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-21 21:33:01 +0100

    Update LuxCore.java

--- src/main/java/net/lux/LuxCore.java
+++ src/main/java/net/lux/LuxCore.java
@@ -3,6 +3,8 @@ package net.lux.core;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import net.fabricmc.api.ModInitializer;
+import java.io.File;
+import java.nio.file.Paths;
 
 public class LuxCore implements ModInitializer {
     public static final String MOD_ID = "lux";
@@ -11,10 +13,26 @@ public class LuxCore implements ModInitializer {
     @Override
     public void onInitialize() {
         LOGGER.info("========================================");
-        LOGGER.info("   Lux Loader is initializing...       ");
-        LOGGER.info("   Developed by: dabdob-craft          ");
-        LOGGER.info("   Status: High-Performance Mode ON    ");
+        LOGGER.info("        LuxLoader Core Loading          ");
         LOGGER.info("========================================");
 
+        discoverMods();
+    }
+
+    private void discoverMods() {
+        File modsFolder = new File(Paths.get("").toAbsolutePath().toString(), "mods");
+        
+        if (modsFolder.exists() && modsFolder.isDirectory()) {
+            File[] files = modsFolder.listFiles((dir, name) -> name.endsWith(".jar"));
+            
+            if (files != null) {
+                LOGGER.info("LuxLoader found {} potential mods.", files.length);
+                for (File file : files) {
+                    LOGGER.info("-> Discovered mod: {}", file.getName());
+                }
+            }
+        } else {
+            LOGGER.warn("Mods folder not found at: {}", modsFolder.getAbsolutePath());
+        }
     }
 }

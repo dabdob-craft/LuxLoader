b4fbff2368a56bd066783742272d82888d11ab4b
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-21 18:43:03 +0100

    Update LuxCore.java

--- src/main/java/net/lux/LuxCore.java
+++ src/main/java/net/lux/LuxCore.java
@@ -1,10 +1,20 @@
-package net.lux;
+package net.lux.core;
 
-public class LuxCore {
-    public static void init() {
-        System.out.println("***************************************");
-        System.out.println("* Lux Loader is Initializing...  *");
-        System.out.println("* Status: High Performance       *");
-        System.out.println("***************************************");
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import net.fabricmc.api.ModInitializer;
+
+public class LuxCore implements ModInitializer {
+    public static final String MOD_ID = "lux";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("========================================");
+        LOGGER.info("   Lux Loader is initializing...       ");
+        LOGGER.info("   Developed by: dabdob-craft          ");
+        LOGGER.info("   Status: High-Performance Mode ON    ");
+        LOGGER.info("========================================");
+
     }
 }

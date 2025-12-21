d2e7e7dca2402a8e9b157fe27afed6d96033903c
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-21 23:11:42 +0100

    Create LuxConfig.java

---
+++ src/main/java/net/lux/core/LuxConfig.java
@@ -0,0 +1,7 @@
+package net.lux.core;
+
+public class LuxConfig {
+    public boolean showModCount = true;
+    public boolean enableFabricCompatibility = true;
+    public String loaderName = "LuxLoader";
+}

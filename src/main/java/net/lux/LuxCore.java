ebe4782b2dec93bd9d27375ae561596e7af5ca4a
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-21 15:52:28 +0100

    Create LuxCore.java

---
+++ src/main/java/net/lux/LuxCore.java
@@ -0,0 +1,10 @@
+package net.lux;
+
+public class LuxCore {
+    public static void init() {
+        System.out.println("***************************************");
+        System.out.println("* Lux Loader is Initializing...  *");
+        System.out.println("* Status: High Performance       *");
+        System.out.println("***************************************");
+    }
+}

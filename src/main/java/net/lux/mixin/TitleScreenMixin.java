4be7af0c73e6a367eb1c048affb7002828c459e3
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-21 22:51:59 +0100

    Update TitleScreenMixin.java

--- src/main/java/net/lux/mixin/TitleScreenMixin.java
+++ src/main/java/net/lux/mixin/TitleScreenMixin.java
@@ -1,9 +1,9 @@
 package net.lux.mixin;
 
 import net.lux.core.ModManager;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.DrawContext;
-import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,13 +13,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class TitleScreenMixin {
     
     @Inject(at = @At("TAIL"), method = "render")
-    private void renderLuxInfo(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+    private void renderLuxInfo(GuiGraphics context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
         String label = "LuxLoader: " + ModManager.getModCount() + " Mods Loaded";
         
-        var font = MinecraftClient.getInstance().textRenderer;
+        var font = Minecraft.getInstance().font;
         
         int screenHeight = ((TitleScreen)(Object)this).height;
         
-        context.drawTextWithShadow(font, label, 2, screenHeight - 10, 0xFFFFFF);
+        context.drawString(font, label, 2, screenHeight - 10, 0xFFFFFF, true);
     }
 }

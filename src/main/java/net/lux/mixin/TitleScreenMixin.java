066ca163284ba580e1658f0931b9a8c8029d0740
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-21 22:37:51 +0100

    Update TitleScreenMixin.java

--- src/main/java/net/lux/mixin/TitleScreenMixin.java
+++ src/main/java/net/lux/mixin/TitleScreenMixin.java
@@ -1,5 +1,7 @@
 package net.lux.mixin;
 
+import net.lux.core.ModManager;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,8 +10,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
-    @Inject(at = @At("HEAD"), method = "init()V")
-    private void init(CallbackInfo info) {
-        net.lux.core.LuxCore.LOGGER.info("Lux has successfully injected into the Title Screen!");
+    
+    @Inject(at = @At("TAIL"), method = "render")
+    private void renderLuxInfo(GuiGraphics context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        String label = "LuxLoader: " + ModManager.getModCount() + " Mods Loaded";
+        
+        int screenHeight = ((TitleScreen)(Object)this).height;
+        context.drawString(((TitleScreen)(Object)this).getMinecraft().font, label, 2, screenHeight - 10, 0xFFFFFF, true);
     }
 }

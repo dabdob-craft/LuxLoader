a23f54eb159bb0ab5ffe69a752a6b5765987d05e
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-21 16:06:57 +0100

    Create TitleScreenMixin.java

---
+++ src/main/java/net/lux/mixin/TitleScreenMixin.java
@@ -0,0 +1,17 @@
+package net.lux.mixin;
+
+import net.minecraft.client.gui.screens.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class TitleScreenMixin {
+    
+    @Inject(at = @At("HEAD"), method = "init()V")
+    private void init(CallbackInfo info) {
+        System.out.println("[Lux] Successfully injected into Title Screen");
+    }
+
+}

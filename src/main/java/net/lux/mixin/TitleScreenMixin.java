9f1c65494e79d41023aa1cb88b0a12382f47f455
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-21 18:44:03 +0100

    Update TitleScreenMixin.java

--- src/main/java/net/lux/mixin/TitleScreenMixin.java
+++ src/main/java/net/lux/mixin/TitleScreenMixin.java
@@ -8,10 +8,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin {
-    
     @Inject(at = @At("HEAD"), method = "init()V")
     private void init(CallbackInfo info) {
-        System.out.println("[Lux] Successfully injected into Title Screen");
+        net.lux.core.LuxCore.LOGGER.info("Lux has successfully injected into the Title Screen!");
     }
-
 }

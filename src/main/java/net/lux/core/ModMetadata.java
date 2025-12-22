43e9acc11c7bac7ebdfdf71a2ac2be3f6ee7ff43
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-22 14:23:12 +0100

    Update ModMetadata.java

--- src/main/java/net/lux/core/ModMetadata.java
+++ src/main/java/net/lux/core/ModMetadata.java
@@ -4,18 +4,19 @@ public class ModMetadata {
     public String id;
     public String name;
     public String version;
+    public String description;
+    public String author;
 
-    public ModMetadata(String id, String name, String version) {
+    public ModMetadata(String id, String name, String version, String description, String author) {
         this.id = id;
         this.name = name;
         this.version = version;
+        this.description = description;
+        this.author = author;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getVersion() {
-        return version;
-    }
+    public String getName() { return name; }
+    public String getVersion() { return version; }
+    public String getDescription() { return description; }
+    public String getAuthor() { return author; }
 }

913077997ee1f855c5c751217dc4c6880718baed
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-22 18:28:34 +0100

    Create LuxLauncher.java

---
+++ src/main/java/net/lux/launcher/LuxLauncher.java
@@ -0,0 +1,17 @@
+package net.lux.launcher;
+
+import java.lang.instrument.Instrumentation;
+
+public class LuxLauncher {
+    public static void premain(String agentArgs, Instrumentation inst) {
+        System.out.println("###################################");
+        System.out.println("   LuxLoader is starting STANDALONE");
+        System.out.println("            Just LUX!              ");
+        System.out.println("###################################");
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Please run this as a Java Agent via Minecraft Launcher.");
+    }
+}

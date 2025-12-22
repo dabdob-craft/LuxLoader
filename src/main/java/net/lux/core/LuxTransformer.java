e7f4cd22dc24b0011f30c54d59aa50be91d6aefd
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-22 18:42:50 +0100

    Update LuxTransformer.java

--- src/main/java/net/lux/core/LuxTransformer.java
+++ src/main/java/net/lux/core/LuxTransformer.java
@@ -1,8 +1,9 @@
 package net.lux.core;
 
+import org.objectweb.asm.*;
+import org.objectweb.asm.tree.*;
 import java.lang.instrument.ClassFileTransformer;
 import java.security.ProtectionDomain;
-import org.objectweb.asm.*;
 
 public class LuxTransformer implements ClassFileTransformer {
     @Override
@@ -10,9 +11,28 @@ public class LuxTransformer implements ClassFileTransformer {
                             ProtectionDomain protectionDomain, byte[] classfileBuffer) {
         
         if (className.equals("net/minecraft/client/gui/screens/TitleScreen")) {
-            System.out.println("[Lux] Modifying TitleScreen...");
-            return modifyBytecode(classfileBuffer);
+            return modifyTitleScreen(classfileBuffer);
         }
         return classfileBuffer;
+    }
+
+    private byte[] modifyTitleScreen(byte[] basicClass) {
+        ClassReader reader = new ClassReader(basicClass);
+        ClassNode node = new ClassNode();
+        reader.accept(node, 0);
+
+        for (MethodNode method : node.methods) {
+            if (method.name.equals("init") || method.name.equals("m_7856_")) { 
+                System.out.println("[Lux] Injecting Button into TitleScreen!");
+                
+                InsnList instructions = new InsnList();
+                
+                method.instructions.insert(instructions);
+            }
+        }
+
+        ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+        node.accept(writer);
+        return writer.toByteArray();
     }
 }

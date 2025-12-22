0db57fd91f2bf0022e4bf468adbcd0ae209eec48
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-22 14:24:46 +0100

    Update ModListWidget.java

--- src/main/java/net/lux/gui/ModListWidget.java
+++ src/main/java/net/lux/gui/ModListWidget.java
@@ -8,17 +8,26 @@ import net.minecraft.client.gui.components.ObjectSelectionList;
 import net.minecraft.network.chat.Component;
 
 public class ModListWidget extends ObjectSelectionList<ModListWidget.ModEntry> {
+    private final ModListScreen parent;
 
-    public ModListWidget(Minecraft client, int width, int height, int top, int bottom) {
+    public ModListWidget(ModListScreen parent, Minecraft client, int width, int height, int top, int bottom) {
         super(client, width, height, top, bottom, 36);
-        
+        this.parent = parent;
         for (ModMetadata mod : ModManager.getLoadedMods()) {
             this.addEntry(new ModEntry(mod));
         }
     }
 
+    @Override
+    public void setSelected(ModEntry entry) {
+        super.setSelected(entry);
+        if (entry != null) {
+            parent.setSelectedMod(entry.metadata);
+        }
+    }
+
     public class ModEntry extends ObjectSelectionList.Entry<ModEntry> {
-        private final ModMetadata metadata;
+        protected final ModMetadata metadata;
 
         public ModEntry(ModMetadata metadata) {
             this.metadata = metadata;
@@ -28,6 +37,12 @@ public class ModListWidget extends ObjectSelectionList<ModListWidget.ModEntry> {
         public void render(GuiGraphics context, int index, int y, int x, int entryWidth, int entryHeight, int mouseX, int mouseY, boolean hovered, float tickDelta) {
             context.drawString(Minecraft.getInstance().font, metadata.getName(), x + 5, y + 5, 0xFFFFFF, true);
             context.drawString(Minecraft.getInstance().font, "v" + metadata.getVersion(), x + 5, y + 17, 0xAAAAAA, false);
+        }
+
+        @Override
+        public boolean mouseClicked(double mouseX, double mouseY, int button) {
+            ModListWidget.this.setSelected(this);
+            return true;
         }
 
         @Override

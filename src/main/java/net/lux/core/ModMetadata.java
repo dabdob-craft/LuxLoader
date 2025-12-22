2ec6137824c488d99bfa206de73d0680a8d367d4
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-22 15:54:08 +0100

    Update ModMetadata.java

--- src/main/java/net/lux/core/ModMetadata.java
+++ src/main/java/net/lux/core/ModMetadata.java
@@ -1,11 +1,14 @@
 package net.lux.core;
 
+import net.minecraft.resources.ResourceLocation;
+
 public class ModMetadata {
     public String id;
     public String name;
     public String version;
     public String description;
     public String author;
+    public ResourceLocation icon;
 
     public ModMetadata(String id, String name, String version, String description, String author) {
         this.id = id;
@@ -13,6 +16,7 @@ public class ModMetadata {
         this.version = version;
         this.description = description;
         this.author = author;
+        this.icon = new ResourceLocation("lux", "textures/gui/icon.png");
     }
 
     public String getId() { return id; }
@@ -20,4 +24,5 @@ public class ModMetadata {
     public String getVersion() { return version; }
     public String getDescription() { return description; }
     public String getAuthor() { return author; }
+    public ResourceLocation getIcon() { return icon; }
 }

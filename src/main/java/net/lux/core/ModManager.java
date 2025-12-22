14f2cb5a0cd4a6799ad5c2fa3697ff91846a4fe4
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-22 17:00:54 +0100

    Update ModManager.java

--- src/main/java/net/lux/core/ModManager.java
+++ src/main/java/net/lux/core/ModManager.java
@@ -6,6 +6,21 @@ import java.util.List;
 public class ModManager {
     private static final List<ModMetadata> LOADED_MODS = new ArrayList<>();
 
+    public static void loadAndInvoke(File modFile) {
+        try {
+            LuxClassLoader classLoader = new LuxClassLoader(ModManager.class.getClassLoader());
+            classLoader.addModFolder(new File("mods"));
+        
+            String mainClassPath = "net.example.mod.ExampleMod"; 
+            Class<?> modClass = Class.forName(mainClassPath, true, classLoader);
+            modClass.getDeclaredMethod("onInitialize").invoke(modClass.getConstructor().newInstance());
+        
+        } catch (Exception e) {
+            System.err.println("Failed to inject mod: " + modFile.getName());
+        }
+    }
+
+
     public static void addMod(ModMetadata mod) {
         LOADED_MODS.add(mod);
     }

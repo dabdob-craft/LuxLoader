278dfa11d00e887acf6df1fca986691362ccc737
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-22 18:00:44 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,46 +1,28 @@
 plugins {
-    java
-    id("fabric-loom") version "1.4-SNAPSHOT"
-    id("maven-publish")
+    id 'java'
 }
 
-group = "net.lux"
-version = "0.1.0-alpha"
+group = 'net.lux'
+version = '1.0.0'
 
 repositories {
     mavenCentral()
-    maven("https://maven.fabricmc.net/")
-    maven("https://repo.spongepowered.org/repository/maven-public/") 
+    maven { url 'https://libraries.minecraft.net/' }
 }
 
 dependencies {
-    minecraft("com.mojang:minecraft:1.20.1")
-    mappings(loom.officialMojangMappings())
-
-    modImplementation("net.fabricmc:fabric-loader:0.15.7")
-
-    implementation("org.spongepowered:mixin:0.8.5")
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation 'org.ow2.asm:asm:9.5'
+    implementation 'org.ow2.asm:asm-tree:9.5'
+    
+    implementation 'com.google.code.gson:gson:2.10.1'
 }
 
-tasks.withType<JavaCompile> {
-    options.release.set(17)
-}
-
-loom {
-    accessWidenerPath.set(file("src/main/resources/lux.accesswidener"))
-}
-
-tasks.jar {
+jar {
     manifest {
         attributes(
-            "Main-Class" to "net.lux.core.LuxCore",
-            "Implementation-Title" to "LuxLoader",
-            "Implementation-Version" to project.version,
-            "Implementation-Vendor" to "dabdob-craft",
-            "Prerelease" to "true",
-            "Multi-Release" to true,
-            "Build-Jdk-Spec" to "17" 
+            'Main-Class': 'net.lux.launcher.LuxLauncher',
+            'Premain-Class': 'net.lux.launcher.LuxLauncher', // لتشغيل اللودر كـ Java Agent
+            'Can-Retransform-Classes': 'true'
         )
     }
 }

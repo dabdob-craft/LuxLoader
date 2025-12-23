87e5c58e84f1f1754d9903cc6589911d475a87ca
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-23 18:48:31 +0100

    Update LuxDecompiler.java

--- lux-loom/src/main/java/net/lux/loom/LuxDecompiler.java
+++ lux-loom/src/main/java/net/lux/loom/LuxDecompiler.java
@@ -1,13 +1,23 @@
 package net.lux.loom;
 
-import org.gradle.api.Project;
+import org.jetbrains.java.decompiler.main.extern.IFernflowerPreferences;
+import org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler;
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class LuxDecompiler {
-    public static void decompile(Project project, File inputJar, File outputSrc) {
-        System.out.println("[LuxDecompiler] Starting to decompile Minecraft...");
-        
-      
-        System.out.println("[LuxDecompiler] Decompilation finished: " + outputSrc.getName());
+    public static void decompile(File inputJar, File outputDir) {
+        System.out.println("[LuxDecompiler] Decompiling Minecraft... Please wait.");
+
+        Map<String, Object> options = new HashMap<>();
+        options.put(IFernflowerPreferences.DECOMPILE_GENERIC_SIGNATURES, "1");
+        options.put(IFernflowerPreferences.INDENT_STRING, "    ");
+
+        ConsoleDecompiler decompiler = new ConsoleDecompiler(outputDir, options);
+        decompiler.addSource(inputJar);
+        decompiler.decompileContext();
+
+        System.out.println("[LuxDecompiler] Successfully decompiled to: " + outputDir.getPath());
     }
 }

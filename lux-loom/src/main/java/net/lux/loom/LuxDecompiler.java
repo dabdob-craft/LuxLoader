3c5448ce0cccb2550aea0786d1091db18cd59407
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-23 21:03:10 +0100

    Update LuxDecompiler.java

--- lux-loom/src/main/java/net/lux/loom/LuxDecompiler.java
+++ lux-loom/src/main/java/net/lux/loom/LuxDecompiler.java
@@ -1,11 +1,14 @@
 package net.lux.loom;
 
 import org.jetbrains.java.decompiler.main.extern.IFernflowerPreferences;
-import org.jetbrains.java.decompiler.main.decompiler.ConsoleDecompiler;
+import org.jetbrains.java.decompiler.main.Fernflower;
 import org.jetbrains.java.decompiler.main.decompiler.PrintStreamLogger;
+import org.jetbrains.java.decompiler.main.extern.IBytecodeProvider;
+import org.jetbrains.java.decompiler.main.extern.IResultSaver;
 import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
+import java.nio.file.Files;
 
 public class LuxDecompiler {
     public static void decompile(File inputJar, File outputDir) {
@@ -17,10 +20,12 @@ public class LuxDecompiler {
 
         PrintStreamLogger logger = new PrintStreamLogger(System.out);
         
-        ConsoleDecompiler decompiler = new ConsoleDecompiler(outputDir, options, logger);
-        
-        decompiler.addSource(inputJar);
-        decompiler.decompileContext();
+        IResultSaver saver = new LuxResultSaver(outputDir);
+        IBytecodeProvider provider = (externalPath, internalPath) -> Files.readAllBytes(new File(externalPath).toPath());
+
+        Fernflower engine = new Fernflower(provider, saver, options, logger);
+        engine.addSource(inputJar);
+        engine.decompileContext();
 
         System.out.println("[LuxDecompiler] Successfully decompiled to: " + outputDir.getPath());
     }

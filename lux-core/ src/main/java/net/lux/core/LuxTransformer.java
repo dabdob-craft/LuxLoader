9f412c76a8edbb92b61cc126a6d6337f44ef2191
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-23 23:21:36 +0100

    Create LuxTransformer.java

---
+++ lux-core/ src/main/java/net/lux/core/LuxTransformer.java
@@ -0,0 +1,40 @@
+package net.lux.core;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.tree.*;
+import org.objectweb.asm.Opcodes;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.security.ProtectionDomain;
+
+public class LuxTransformer implements ClassFileTransformer {
+    @Override
+    public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined,
+                            ProtectionDomain protectionDomain, byte[] classfileBuffer) {
+        
+        if (className.equals("net/minecraft/client/MinecraftClient")) {
+            System.out.println("[LuxCore] Patching MinecraftClient...");
+            
+            ClassReader reader = new ClassReader(classfileBuffer);
+            ClassNode node = new ClassNode();
+            reader.accept(node, 0);
+
+            for (MethodNode method : node.methods) {
+                if (method.name.equals("<init>")) {
+                    InsnList insns = new InsnList();
+                    insns.add(new FieldInsnNode(Opcodes.GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;"));
+                    insns.add(new LdcInsnNode(">>> LuxLoader: Game is starting with custom injection! <<<"));
+                    insns.add(new MethodInsnNode(Opcodes.INVOKEVIRTUAL, "java/io/PrintStream", "println", "(Ljava/lang/String;)V", false));
+                    
+                    method.instructions.insert(insns);
+                }
+            }
+
+            ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
+            node.accept(writer);
+            return writer.toByteArray();
+        }
+        return null;
+    }
+}

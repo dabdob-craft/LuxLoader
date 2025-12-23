4db783301d839c7e1bf0f032798e26124802fae0
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-23 13:28:15 +0100

    Create LuxLauncher.java

---
+++ lux-core/ src/main/java/net/lux/launcher/LuxLauncher.java
@@ -0,0 +1,16 @@
+package net.lux.launcher;
+
+import java.lang.instrument.Instrumentation;
+import net.lux.core.ModManager;
+
+public class LuxLauncher {
+    public static void premain(String agentArgs, Instrumentation inst) {
+        System.out.println("====================================");
+        System.out.println("     LUX LOADER: STARTING UP        ");
+        System.out.println("            Just LUX.               ");
+        System.out.println("====================================");
+
+        inst.addTransformer(new LuxTransformer());
+
+    }
+}

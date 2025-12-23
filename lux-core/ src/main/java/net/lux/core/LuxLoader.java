669602c4265c24d6162ea2abe40d44003564a115
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-23 23:10:40 +0100

    Update LuxLoader.java

--- lux-core/ src/main/java/net/lux/core/LuxLoader.java
+++ lux-core/ src/main/java/net/lux/core/LuxLoader.java
@@ -6,23 +6,20 @@ import java.util.List;
 
 public class LuxLoader {
     private static final File MODS_DIR = new File("mods");
-    private static final List<Object> LOADED_MODS = new ArrayList<>();
 
     public static void init() {
-        System.out.println("[LuxLoader] Initializing independent mod loading system...");
-        
+        System.out.println("[LuxLoader-Core] Searching for mods in " + MODS_DIR.getAbsolutePath());
+
         if (!MODS_DIR.exists()) {
             MODS_DIR.mkdirs();
+            return;
         }
 
-        File[] files = MODS_DIR.listFiles((dir, name) -> name.endsWith(".jar"));
-        
-        if (files != null) {
-            for (File file : files) {
-                System.out.println("[LuxLoader] Found potential mod: " + file.getName());
+        File[] mods = MODS_DIR.listFiles((dir, name) -> name.endsWith(".jar"));
+        if (mods != null) {
+            for (File mod : mods) {
+                System.out.println("[LuxLoader] Found mod: " + mod.getName());
             }
         }
-        
-        System.out.println("[LuxLoader] Total mods discovered: " + (files != null ? files.length : 0));
     }
 }

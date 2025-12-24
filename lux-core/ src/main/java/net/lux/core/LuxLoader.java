fe6c9a330d370f61fd8cf2899e5f957ff81e6e3e
dabdob-craft <dabdobcraft2@gmail.com>
2025-12-24 13:18:46 +0100

    Update LuxLoader.java

--- lux-core/ src/main/java/net/lux/core/LuxLoader.java
+++ lux-core/ src/main/java/net/lux/core/LuxLoader.java
@@ -4,12 +4,14 @@ import com.google.gson.Gson;
 import java.io.*;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.Enumeration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 public class LuxLoader {
     private static final Gson GSON = new Gson();
+    private static final List<Object> LOADED_MODS = new ArrayList<>();
 
     public static void init() {
         File modsDir = new File("mods");
@@ -25,15 +27,16 @@ public class LuxLoader {
                     InputStream is = jar.getInputStream(entry);
                     ModMetadata meta = GSON.fromJson(new InputStreamReader(is), ModMetadata.class);
                     
-                    System.out.println("[Lux] Loading mod: " + meta.id + " v" + meta.version);
+                    System.out.println("[Lux] Loading mod: " + meta.id);
 
                     URLClassLoader child = new URLClassLoader(
                         new URL[]{file.toURI().toURL()},
                         LuxLoader.class.getClassLoader()
                     );
 
-                    Class<?> modClass = Class.forName(meta.entrypoint, true, child);
-                    modClass.getDeclaredConstructor().newInstance();
+                    Class<?> modClass = Class.forName(meta.entrypoint, true, chil
+                    Object modInstance = modClass.getDeclaredConstructor().newInstance();
+                    LOADED_MODS.add(modInstance);
                     
                     System.out.println("[Lux] Mod " + meta.id + " initialized!");
                 }
@@ -41,5 +44,8 @@ public class LuxLoader {
                 System.err.println("[Lux] Failed to load mod " + file.getName() + ": " + e.getMessage());
             }
         }
+    }
+
+    public static void onTick() {
     }
 }
